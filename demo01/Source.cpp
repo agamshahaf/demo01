370ed4d2734408ff79b20e6aeb1f94e0ac6a807e
אגם שחף <agamy.shahaf@gmail.com>
2022-02-03 19:12:20 +0200

    Add project files.

---
+++ demo01/Source.cpp
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <glut.h>
+#include <GLUT.h>
+#include <stdio.h>
+#include <math.h>
+
+// add const pi as float
+
+void DrawCircle(float cx, float cy, float r, int num_segments)
+{
+    glBegin(GL_LINE_LOOP);
+    for (int ii = 0; ii < num_segments; ii++)
+    {
+        float theta = 2.0f * 3.1415926f * float(ii) / float(num_segments);//get the current angle
+
+        float x = r * cosf(theta);//calculate the x component
+        float y = r * sinf(theta);//calculate the y component
+
+        glVertex2f(x + cx, y + cy);//output vertex
+
+    }
+    glEnd();
+}
+
+
+void Draw() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    // draw lines
+    glColor3f(0.0, 1.0, 0.0);
+    glBegin(GL_LINES);
+    //add lines
+    glEnd();
+    // draw circles
+    glColor3f(0.0, 1.0, 0.0);
+    for (int j = 50; j < 201; j = j + 50)
+    {
+        DrawCircle(0.0, 0.0, j, 72);
+    }
+    // draw beam
+    //glColor3f(0.0, 1.0, 0.0); - change color
+    //glBegin(GL_LINES);
+    //glVertex3f(0, 0, 0);
+   // glVertex3f(0, 0, 0);
+   // glEnd();
+
+
+    glFlush();
+}
+
+void idle() {
+    // t
+    glutPostRedisplay();
+}
+
+void Initialize() {
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-250.0, 250.0, -250.0, 250.0, 0.0,15.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+int main(int iArgc, char** cppArgv) {
+    glutInit(&iArgc, cppArgv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(20, 20);
+    glutCreateWindow("авн");
+    Initialize();
+    glutDisplayFunc(Draw);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glutMainLoop();
+    return 0;
+}
